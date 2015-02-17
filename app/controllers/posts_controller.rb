92aab05617fcd22df5a14afc4945dc97bb9a79bb
Salma M. Ali <bluet.salma@gmail.com>
2015-02-17 21:21:02 +0300

    Delete/Showing Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,14 @@
 class PostsController < ApplicationController
+def change 
+	add_column :user_id
+end
+
+def index 
+	@posts = Post.all
+end
+
+def show 
+	@post = Post.find(params[:user_id])
+end
 
 end
--- db/migrate/20150217132023_create_posts.rb
+++ db/migrate/20150217132023_create_posts.rb
@@ -2,13 +2,10 @@ class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
       t.string :title
-      
       t.text :paragraph
-      
-
-
-
+      t.references  :user
       t.timestamps null: false
+      add_column :user_id
     end
   end
 end
