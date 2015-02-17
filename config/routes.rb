5242c1ba41d113582976e907c8cc10f2cd22c533
Salma M. Ali <bluet.salma@gmail.com>
2015-02-17 22:22:48 +0300

    Categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+   resources :posts
   #posts GET /posts(.:format)  posts#index
   root 'posts#index'
   # The priority is based upon order of creation: first created -> highest priority.
